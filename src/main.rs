5904a76faa8de8d3ea009b9c4e6ad87402a6ac93
Hao CHEN <hao.chen@cri-paris.org>
2025-06-20 11:29:16 +0200

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Search {} in {}", config.query, config.file_path);
 
@@ -18,9 +22,9 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Self, &'static str > {
+    fn build(args: &[String]) -> Result<Self, &'static str > {
         if args.len() < 3 {
-            return Err("Not enough arguments. Please specify search query and file path.");
+            return Err("Not enough arguments.");
         }
         let query = args.get(1).unwrap().clone();
         let file_path = args.get(2).unwrap().clone();
