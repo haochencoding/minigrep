7e2b90ac3d8abb4a5cb4dfd82f401a1494e2db41
Hao CHEN <hao.chen@cri-paris.org>
2025-06-20 13:10:10 +0200

    Set up env

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool
 }
 
 impl Config {
@@ -13,15 +15,20 @@ impl Config {
         }
         let query = args.get(1).unwrap().clone();
         let file_path = args.get(2).unwrap().clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Self { query, file_path })
+        Ok(Self { query, file_path, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    let results = search(&config.query, &contents);
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
     for line in results {
         println!("{line}");
