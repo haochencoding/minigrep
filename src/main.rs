f7548e6765d08d4cbe5adfb7ddc6963281c44a85
Hao CHEN <hao.chen@cri-paris.org>
2025-06-20 11:11:32 +0200

    First function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(&args);
+
+    println!("Search {} in {}", config.query, config.file_path);
+
+    let contents = fs::read_to_string(config.file_path).unwrap();
+    println!("{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args.get(1).unwrap().clone();
+    let file_path = args.get(2).unwrap().clone();
+
+    Config { query, file_path }
 }
